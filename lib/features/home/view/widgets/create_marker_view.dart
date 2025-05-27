2c96076c19fe8022c64b3f54bfd14d9dd9cf9d1b
Mohamedmh3 <syo358814@gmail.com>
2025-05-27 18:13:21 +0300

    create marker view

--- lib/features/home/view/widgets/create_marker_view.dart
+++ lib/features/home/view/widgets/create_marker_view.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:recoding_platform_project/features/home/view/widgets/dropdown_button_widget.dart';
+import 'package:recoding_platform_project/features/home/view/widgets/drop_down_sub_aspect.dart';
+import 'package:recoding_platform_project/src/components/auth_button.dart';
 import 'package:recoding_platform_project/src/components/header.dart';
 import 'package:recoding_platform_project/src/components/input_text_form_field.dart';
-import 'package:recoding_platform_project/src/components/outline_input_text_form.dart';
-import 'package:recoding_platform_project/src/components/rounded_rectangle_button.dart';
 import 'package:recoding_platform_project/src/themes/app_colors.dart';
 
 class CreateMarkerView extends StatelessWidget {
@@ -15,7 +14,6 @@ class CreateMarkerView extends StatelessWidget {
     return Scaffold(
         body: SingleChildScrollView(
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Header(
             headerText: "Create Marker",
@@ -26,103 +24,103 @@ class CreateMarkerView extends StatelessWidget {
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 50.w),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('Marker Name'),
-                InputTextFormField(
-                  hintText: "Aspect",
-                  prefixIcon: Icon(Icons.abc),
-                ),
-                DropdownButtonFormField<String>(
-                  dropdownColor: Colors.white,
-                  icon: Icon(
-                    Icons.keyboard_arrow_down_outlined,
-                    color: Colors.grey,
-                  ),
-                  decoration: InputDecoration(
-                    hintText: 'Filter by Team',
-                    hintStyle: Theme.of(context)
-                        .textTheme
-                        .labelMedium
-                        ?.copyWith(fontSize: 16.r, fontWeight: FontWeight.w300),
-                    contentPadding: const EdgeInsets.symmetric(vertical: 12),
-                    border: const UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        width: 3.73,
-                        color: AppColors.black015,
-                      ),
-                    ),
-                    enabledBorder: const UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        width: 3.73,
-                        color: AppColors.black015,
-                      ),
-                    ),
-                    focusedBorder: const UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        width: 3.73,
-                        color: AppColors.black015,
-                      ),
-                    ),
-                    prefixIcon: Icon(
-                      Icons.category_outlined,
-                      size: 20.r,
-                      color: Colors.grey,
-                    ),
-                  ),
+                Text(
+                  '[Marker Name]',
                   style: Theme.of(context)
                       .textTheme
                       .labelMedium
-                      ?.copyWith(fontSize: 16.r, fontWeight: FontWeight.w300),
-                  items: ['Category A', 'Category B', 'Category C']
-                      .map((cat) =>
-                          DropdownMenuItem(value: cat, child: Text(cat)))
-                      .toList(),
+                      ?.copyWith(fontSize: 25.sp),
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                InputTextFormField(
+                  hintText: "Aspect",
+                  prefixIcon: Icon(Icons.sync, color: Color(0xff787878)),
+                ),
+                SizedBox(
+                  height: 32.h,
+                ),
+                DropDownSubAspect(
                   onChanged: (value) {},
                 ),
                 SizedBox(
-                  height: 24.h,
+                  height: 32.h,
+                ),
+                InputTextFormField(
+                  hintText: "Category",
+                  prefixIcon:
+                      Icon(Icons.category_outlined, color: Color(0xff787878)),
                 ),
                 SizedBox(
-                  height: 10.h,
+                  height: 32.h,
                 ),
-                OutlineInputTextForm(
-                  hintText: "Damascus Marker",
-                  suffixIcon: Icons.edit_outlined,
-                ),
-                SizedBox(
-                  height: 10.h,
-                ),
-                OutlineInputTextForm(
-                  hintText: "Social Marker",
-                  suffixIcon: Icons.edit_outlined,
+                InputTextFormField(
+                  hintText: "Location name",
+                  prefixIcon: Icon(Icons.input, color: Color(0xff787878)),
                 ),
                 SizedBox(
-                  height: 10.h,
+                  height: 32.h,
                 ),
-                OutlineInputTextForm(
-                  hintText: "Data Collection Marker",
-                  suffixIcon: Icons.edit_outlined,
-                ),
-                SizedBox(
-                  height: 10.h,
-                ),
-                OutlineInputTextForm(
-                  hintText: "Something Marker",
-                  suffixIcon: Icons.edit_outlined,
+                InputTextFormField(
+                  hintText: "Description",
+                  prefixIcon: Icon(Icons.description_outlined,
+                      color: Color(0xff787878)),
                 ),
                 SizedBox(
-                  height: 10.h,
+                  height: 32.h,
                 ),
-                OutlineInputTextForm(
-                  hintText: "Example Marker",
-                  suffixIcon: Icons.edit_outlined,
+                InputTextFormField(
+                  hintText: "Upload images",
+                  prefixIcon: Icon(
+                    Icons.cloud_upload_outlined,
+                    color: Color(0xff787878),
+                  ),
+                  suffixIcon: Icon(Icons.upload_rounded),
                 ),
                 SizedBox(
-                  height: 150.h,
+                  height: 30.h,
                 ),
-                RoundedRectangleButton(
-                  onPressed: () {},
-                  text: "Create New Marker",
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AuthButton(
+                      onPressed: () {},
+                      buttonWidth: 152.w,
+                      text: "Create",
+                      textStyle: Theme.of(context)
+                          .textTheme
+                          .labelMedium
+                          ?.copyWith(
+                              fontSize: 18.sp,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                      buttonStyle: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xff6ab3d9),
+                        foregroundColor: AppColors.black073,
+                        elevation: 0,
+                        overlayColor: Color(0xff6ab3d9),
+                        shadowColor: Colors.transparent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                      ),
+                    ),
+                    AuthButton(
+                      onPressed: () {},
+                      text: 'Cancel',
+                      buttonWidth: 152.w,
+                      textStyle: Theme.of(context)
+                          .textTheme
+                          .labelMedium
+                          ?.copyWith(
+                              fontSize: 18.sp,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                    ),
+                  ],
                 ),
                 SizedBox(
                   height: 20,
