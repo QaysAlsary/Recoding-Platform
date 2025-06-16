ef55d0b32f7dca109a5183ecea0bda322f382d2c
Mohamedmh3 <syo358814@gmail.com>
2025-06-16 07:04:49 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:maptiler_flutter/maptiler_flutter.dart';
 import 'package:recoding_platform_project/features/home/bloc/home_bloc.dart';
+import 'package:recoding_platform_project/features/home/models/repo/home_repo.dart';
 import 'package:recoding_platform_project/features/profile/bloc/profile_bloc.dart';
 import 'package:recoding_platform_project/src/di/app_initializer.dart';
 import 'package:recoding_platform_project/src/di/service_locator.dart';
@@ -13,8 +14,12 @@ import 'package:recoding_platform_project/src/themes/app_theme.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await AppInitializer.init();
-  MapTilerConfig.setApiKey('tBWczgtWITcq8rfJeIuA');
+  try {
+    await AppInitializer.init();
+    MapTilerConfig.setApiKey('tBWczgtWITcq8rfJeIuA');
+  } catch (e) {
+    debugPrint('Error during initialization: $e');
+  }
 
   runApp(const MyApp());
 }
@@ -35,7 +40,7 @@ class MyApp extends StatelessWidget {
               create: (context) => getIt<HomeBloc>(),
             ),
             BlocProvider<ProfileBloc>(
-              create: (context) => getIt<ProfileBloc>(),
+              create: (context) => getIt<ProfileBloc>()..add(LoadProfile()),
             ),
           ],
           child: MaterialApp.router(
